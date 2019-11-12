5937c4375bc238be9cb39797607be766c5fe8a3f
Genifer Moreno <dcastro@virtualbeams.com>
2019-11-12 12:54:13 -0500

    add reports download

--- src/app/pages/dashboard/reportes/reportes.component.ts
+++ src/app/pages/dashboard/reportes/reportes.component.ts
@@ -11,7 +11,9 @@ export class ReportesComponent implements OnInit {
   private reportsPath = Constants.URL_SERVER + 'reports/';
 
   public reportsURLS = {
-    born_by_animal: this.reportsPath + 'born_by_animals'
+    born_by_animal: this.reportsPath + 'born_by_animals',
+    born_alive_per_month: this.reportsPath + 'born_alive_per_month',
+    born_dead_per_month: this.reportsPath + 'born_dead_per_month'
   };
 
   constructor() { }
